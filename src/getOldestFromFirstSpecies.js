d68f20406602a6a6977323e910fefc4acd812542
devpedroval <prmval@gmail.com>
2022-12-08 18:47:50 -0300

    requisito 10 completo

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,23 @@
 const data = require('../data/zoo_data');
 
 const getOldestFromFirstSpecies = (id) => {
-  // seu código aqui
+  const retorno = [];
+  const array = data.employees;
+  const laborer = array.find((empregado) => id === empregado.id);
+  const firstAnimalId = laborer.responsibleFor[0];
+  const animal = data.species.find((especie) => especie.id === firstAnimalId).residents;
+  const one = animal.reduce((acc, cur) => {
+    if (acc.age < cur.age) {
+      let older = acc;
+      older = cur;
+      return older;
+    }
+    return acc;
+  });
+  retorno.push(one.name);
+  retorno.push(one.sex);
+  retorno.push(one.age);
+  return retorno;
 };
 
 module.exports = getOldestFromFirstSpecies;
