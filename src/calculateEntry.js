afb448ff7c7458497272d8297f71e9e10babfe36
devpedroval <prmval@gmail.com>
2022-12-08 17:55:12 -0300

    requisito 8 completo

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,32 @@
 const data = require('../data/zoo_data');
 
 const countEntrants = (entrants) => {
-  // seu código aqui
+  const obj = {
+    child: 0,
+    adult: 0,
+    senior: 0,
+  };
+  entrants.forEach((pessoa) => {
+    if (pessoa.age < 18) {
+      obj.child += 1;
+    }
+    if (pessoa.age >= 18 && pessoa.age < 50) {
+      obj.adult += 1;
+    }
+    if (pessoa.age >= 50) {
+      obj.senior += 1;
+    }
+  });
+  return obj;
 };
 
 const calculateEntry = (entrants) => {
-  // seu código aqui
+  const obj = countEntrants(entrants);
+  const seniors = obj.senior * data.prices.senior;
+  const adults = obj.adult * data.prices.adult;
+  const childs = obj.child * data.prices.child;
+  const total = seniors + adults + childs;
+  return total;
 };
 
 module.exports = { calculateEntry, countEntrants };
