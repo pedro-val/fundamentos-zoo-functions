3f9bda9cfab67b741f56632cc4bab9a21593adfa
devpedroval <prmval@gmail.com>
2022-12-08 15:07:27 -0300

    requisito 1 completo

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,18 @@
 const data = require('../data/zoo_data');
 
-const getSpeciesByIds = (ids) => {
-  // seu código aqui
+const getSpeciesByIds = (...ids) => {
+  if (ids.length === 0) {
+    return [];
+  }
+  const retorno = [];
+  ids.forEach((id) => {
+    data.species.forEach((especie) => {
+      if (especie.id === id) {
+        retorno.push(especie);
+      }
+    });
+  });
+  return retorno;
 };
 
 module.exports = getSpeciesByIds;
