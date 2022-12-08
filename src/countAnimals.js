2a83e017d866e9d3b1c0f407ffca5cffa48bcfd1
devpedroval <prmval@gmail.com>
2022-12-08 17:30:02 -0300

    requisito 5 completo com sucesso

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,20 @@
 const data = require('../data/zoo_data');
 
-const countAnimals = (animal) => {
-  // seu código aqui
+const array = data.species;
+
+const countAnimals = (animal = 'default') => {
+  if (animal === 'default') {
+    return array.reduce(((acc, cur) => {
+      acc[cur.name] = cur.residents.length;
+      return acc;
+    }), {});
+  }
+  const unit = array.filter((especie) => especie.name === animal.species);
+  const sexOn = unit[0].residents.filter((sexo) => sexo.sex === animal.sex);
+  if (animal.sex) {
+    return sexOn.length;
+  }
+  return unit[0].residents.length;
 };
 
 module.exports = countAnimals;
