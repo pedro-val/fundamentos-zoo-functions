1a9b337a995bdf2f334e99596d484264f91ec437
devpedroval <prmval@gmail.com>
2022-12-08 15:40:10 -0300

    requisito 3 completo

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,7 +1,13 @@
 const data = require('../data/zoo_data');
 
-const getEmployeeByName = (employeeName) => {
-  // seu código aqui
-};
+const getEmployeeByName = (employeeName) => data.employees.reduce(((acc, cur) => {
+  if (cur.firstName === employeeName || cur.lastName === employeeName) {
+    cur = acc;
+    return acc;
+  }
+  return acc;
+}), {});
+
+console.log(getEmployeeByName('Nigel'));
 
 module.exports = getEmployeeByName;
