1e6373e1742b8b7501e8f57ffd272a47eef9bfce
devpedroval <prmval@gmail.com>
2022-12-08 16:09:41 -0300

    teste requisitos

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -3,7 +3,6 @@ const data = require('../data/zoo_data');
 const getAnimalsOlderThan = (animal, age) => {
   const array = data.species.filter((especie) => especie.name === animal);
   return array[0].residents.every((residente) => residente.age > age);
-
 };
 getAnimalsOlderThan('lions', 20);
 
--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,22 @@
 const data = require('../data/zoo_data');
 
 const isManager = (id) => {
-  // seu código aqui
+  const empregados = data.employees;
+  let boolean = false;
+  empregados.forEach((empregado) => {
+    if (empregado.managers.includes(id)) {
+      boolean = true;
+    }
+  });
+  return boolean;
 };
 
 const getRelatedEmployees = (managerId) => {
-  // seu código aqui
+  if (isManager(managerId) === false) {
+    throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
+  }
+  // const empregados = data.employees;
+  // // const colaboradores = empregados.filter((empregado) => {});
 };
 
 module.exports = { isManager, getRelatedEmployees };
