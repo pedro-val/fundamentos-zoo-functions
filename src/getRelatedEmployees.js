6cb081a42b352a7bc0a708f6cc253e527338e82f
devpedroval <prmval@gmail.com>
2022-12-08 16:24:22 -0300

    requisito 4 completo

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -15,8 +15,12 @@ const getRelatedEmployees = (managerId) => {
   if (isManager(managerId) === false) {
     throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
   }
-  // const empregados = data.employees;
-  // // const colaboradores = empregados.filter((empregado) => {});
+  const empregados = data.employees;
+  const colaboradores = empregados.filter((empregado) => empregado.managers.includes(managerId));
+  const retorno = [];
+  colaboradores.forEach((nome) => {
+    retorno.push(`${nome.firstName} ${nome.lastName}`);
+  });
+  return retorno;
 };
-
 module.exports = { isManager, getRelatedEmployees };
