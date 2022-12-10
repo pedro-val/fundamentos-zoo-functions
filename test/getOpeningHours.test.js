394fa09c9b75668146ba14649a3ec4305b5a4952
devpedroval <prmval@gmail.com>
2022-12-10 09:35:45 -0300

    tentando 100%

--- test/getOpeningHours.test.js
+++ test/getOpeningHours.test.js
@@ -1,14 +1,17 @@
 const getOpeningHours = require('../src/getOpeningHours');
 
+const closed = 'The zoo is closed';
+const open = 'The zoo is open';
+
 describe('Testes da função getOpeningHours', () => {
   it('funcionalidade quando parametros normais', () => {
-    expect(getOpeningHours('Friday', '7:00-PM')).toEqual('The zoo is open');
+    expect(getOpeningHours('Friday', '7:00-PM')).toEqual(open);
   });
   it('testando the zoo is closed', () => {
-    expect(getOpeningHours('Monday', '11:00-PM')).toEqual('The zoo is closed');
+    expect(getOpeningHours('Monday', '11:00-PM')).toEqual(closed);
   });
   it('testando the zoo is closed outro dia da semana', () => {
-    expect(getOpeningHours('Sunday', '11:00-PM')).toEqual('The zoo is closed');
+    expect(getOpeningHours('Sunday', '11:00-PM')).toEqual(closed);
   });
   it('testando erro de dia da semana', () => {
     try {
@@ -58,5 +61,22 @@ describe('Testes da função getOpeningHours', () => {
     } catch (erroMsg) {
       expect(erroMsg.message).toEqual('The hour must be between 0 and 12');
     }
+  });
+  it('testando erro adicional', () => {
+    expect(getOpeningHours('Friday', '12:00-PM')).toEqual(open);
+  });
+  it('testando função sem parametro', () => {
+    expect(getOpeningHours()).toEqual({
+      Tuesday: { open: 8, close: 6 },
+      Wednesday: { open: 8, close: 6 },
+      Thursday: { open: 10, close: 8 },
+      Friday: { open: 10, close: 8 },
+      Saturday: { open: 8, close: 10 },
+      Sunday: { open: 8, close: 8 },
+      Monday: { open: 0, close: 0 },
+    });
+  });
+  it('testando erro adicional para horarios da madrugada', () => {
+    expect(getOpeningHours('Friday', '01:00-AM')).toEqual(closed);
   });
 });
